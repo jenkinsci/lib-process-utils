fbb199ab376e4ebeb7340ffcae2af30df89dbd91
Basil Crow <me@basilcrow.com>
2023-07-25 15:00:17 -0700

    fixups

--- src/main/java/org/jenkinsci/utils/process/ProcessUtils.java
+++ src/main/java/org/jenkinsci/utils/process/ProcessUtils.java
@@ -8,11 +8,7 @@ public class ProcessUtils {
      * Figure out the UNIX process ID from {@link Process}
      */
     public static long getPid(Process p) {
-        try {
-            ProcessHandle handle = p.toHandle();
-            return handle.pid();
-        } catch (UnsupportedOperationException e1) {
-            return -1L;
-        }
+        ProcessHandle handle = p.toHandle();
+        return handle.pid();
     }
 }
